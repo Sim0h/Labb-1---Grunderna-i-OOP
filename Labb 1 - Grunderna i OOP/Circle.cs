1579d9cbdc83d4b8d585fb393f42a12a041d7e02
Simon Ståhl <simon.stahl94@gmail.com>
2023-10-19 12:41:19 +0200

    Add project files.

---
+++ Labb 1 - Grunderna i OOP/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_1___Grunderna_i_OOP
+{
+    internal class Circle
+    {
+        float _pi = 3.141f;
+        int _Radius;
+
+        public Circle(int Radius)
+        {
+            
+            _Radius = Radius;
+        }
+
+        public float Printarea() 
+        {
+           float area = _Radius * _Radius* _pi;
+            return area;
+        }
+
+            
+    }
+}
---
+++ Labb 1 - Grunderna i OOP/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Labb_1___Grunderna_i_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle R5 = new Circle(5);
+            Console.WriteLine(R5.Printarea());
+
+            Circle R6 = new Circle(6);
+            Console.WriteLine(R6.Printarea());
+
+        }
+    }
+}
